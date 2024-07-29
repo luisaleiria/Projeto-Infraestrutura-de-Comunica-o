5b7f7f222cb4aea4b958ad2cd8424fb5c15e4ec9
Luisa Leiria <112179923+luisaleiria@users.noreply.github.com>
2024-07-28 21:16:53 -0300

    Update UDPcliente2.py

--- entrega3/UDPcliente2.py
+++ entrega3/UDPcliente2.py
@@ -1,6 +1,5 @@
 import socket as skt
-import os
-import time
+import threading
 import random
 
 MAX_BUFFER = 1024  # tamanho máximo dos dados
@@ -17,41 +16,40 @@ class RDT:
         self.seq_num = 0 # numero de sequencia inicial
 
     def send(self, addr, msg):
-         #simula a perda de pacotes com a probabilidade definida na parte de cima do codigo
+        # Simula a perda de pacotes com a probabilidade definida na parte de cima do código
         print("entrou RDT")
-        #if random.random() < LOSS_PROBABILITY:
+        # if random.random() < LOSS_PROBABILITY:
         #    print(f"Simulando perda de pacote seq_num: {self.seq_num}")
         #    return  # Simula a perda do pacote
 
-        # Cria um pacote com o numero de sequencia e mensagem
+        # Cria um pacote com o número de sequência e mensagem
         packet = f"{self.seq_num}|".encode('utf-8') + msg
         self.socket.sendto(packet, addr)
-        #print(f"Enviado pacote seq_num: {self.seq_num}")
+        # print(f"Enviado pacote seq_num: {self.seq_num}")
         self.seq_num = 1 - self.seq_num  # alterna entre 0 e 1
 
     def receive(self):
         while True:
-            data, addr = self.socket.recvfrom(self.max_buffer) #recebe os dados
+            data, addr = self.socket.recvfrom(self.max_buffer)  # recebe os dados
             if b'|' not in data:
-                continue #ignora os pacotes que foram mal formados
-            header, msg = data.split(b'|', 1) #separa o cabeçalho da mensagem em si
-            recv_seq_num = int(header.decode('utf-8')) #decodifica a sequencia
+                continue  # ignora os pacotes que foram mal formados
+            header, msg = data.split(b'|', 1)  # separa o cabeçalho da mensagem em si
+            recv_seq_num = int(header.decode('utf-8'))  # decodifica a sequência
             if recv_seq_num == self.seq_num:
-                #envia o ACK confirmando o recebimento
+                # envia o ACK confirmando o recebimento
                 self.socket.sendto(f"ACK{recv_seq_num}".encode('utf-8'), addr)
                 print(f"Recebido e confirmado pacote seq_num: {recv_seq_num}")
-                self.seq_num = 1 - self.seq_num #alterna o num de seq
-                return msg, addr # retorna a mensagem e o endereço pro servidor
-
-
+                self.seq_num = 1 - self.seq_num  # alterna o num de seq
+                return msg, addr  # retorna a mensagem e o endereço pro servidor
 
 
 class Cliente:
     def __init__(self, sckt_family, sckt_type, sckt_binding, max_buffer):
-        self.sckt = skt.socket(sckt_family, sckt_type) #cria o socket
-        self.sckt.bind(sckt_binding) #vincula o socket ao endereço dado
-        self.rdt = RDT(self.sckt, max_buffer) #cria uma instancia de RDT
+        self.sckt = skt.socket(sckt_family, sckt_type)  # cria o socket
+        self.sckt.bind(sckt_binding)  # vincula o socket ao endereço dado
+        self.rdt = RDT(self.sckt, max_buffer)  # cria uma instância de RDT
         self.server_addr = ADDR_TARGET
+        self.running = True
 
     def login(self, username):
         print("ta em login")
@@ -94,32 +92,52 @@ class Cliente:
         msg, _ = self.rdt.receive()
         print(msg.decode('utf-8'))
 
+    def listen_for_messages(self):
+        while self.running:
+            msg, addr = self.rdt.receive()
+            print(f"Mensagem do servidor: {msg.decode('utf-8')}")
+
+    def start_listener(self):
+        self.listener_thread = threading.Thread(target=self.listen_for_messages)
+        self.listener_thread.start()
+
+    def stop_listener(self):
+        self.running = False
+        self.listener_thread.join()
+
+
 def main_cliente():
     client = Cliente(skt.AF_INET, skt.SOCK_DGRAM, ADDR_BIND, MAX_BUFFER)
+    client.start_listener()
 
-    while True:
-        command = input("> ")
-        if command.startswith("login"):
-            print("ta no case login")
-            _, username = command.split()
-            client.login(username)
-        elif command == "logout":
-            client.logout()
-        elif command.startswith("create"):
-            _, name, location, *description = command.split()
-            client.create_accommodation(name, location, ' '.join(description))
-        elif command == "list:myacmd":
-            client.list_my_accommodations()
-        elif command == "list:acmd":
-            client.list_accommodations()
-        elif command == "list:myrsv":
-            client.list_my_reservations()
-        elif command.startswith("book"):
-            _, owner, name, location, day = command.split()
-            client.book_accommodation(owner, name, location, day)
-        elif command.startswith("cancel"):
-            _, owner, name, location, day = command.split()
-            client.cancel_reservation(owner, name, location, day)
+    try:
+        while True:
+            command = input("> ")
+            if command.startswith("login"):
+                print("ta no case login")
+                _, username = command.split()
+                client.login(username)
+            elif command == "logout":
+                client.logout()
+            elif command.startswith("create"):
+                _, name, location, *description = command.split()
+                client.create_accommodation(name, location, ' '.join(description))
+            elif command == "list:myacmd":
+                client.list_my_accommodations()
+            elif command == "list:acmd":
+                client.list_accommodations()
+            elif command == "list:myrsv":
+                client.list_my_reservations()
+            elif command.startswith("book"):
+                _, owner, name, location, day = command.split()
+                client.book_accommodation(owner, name, location, day)
+            elif command.startswith("cancel"):
+                _, owner, name, location, day = command.split()
+                client.cancel_reservation(owner, name, location, day)
+    except KeyboardInterrupt:
+        print("Encerrando o cliente...")
+        client.stop_listener()
+
 
 if __name__ == "__main__":
-    main_cliente() 
+    main_cliente()
